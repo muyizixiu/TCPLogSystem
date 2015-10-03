6cf09569d769cb8f6ea1d1b26472bdafe38ba1e8
root <root@localhost.localdomain>
2015-10-03 06:17:43 -0700

    completing ....

--- server/s.go
+++ server/s.go
@@ -1,14 +1,18 @@
 package server
 
 import (
+	"config"
 	"fmt"
+	"io"
 	"net"
+	"regexp"
 )
 
 var (
 	addr       string
 	port       string
-	onlyserver server
+	onlyServer server
+	HTTP_r     *regexp.Regexp
 )
 
 type server struct {
@@ -17,14 +21,20 @@ type server struct {
 	Data     chan [config.MaxNumberOfData][]byte
 	listener net.Listener
 }
+
+func (s server) GetDataChannel() chan [config.MaxNumberOfData][]byte {
+	return s.Data
+}
+
 type connHandler interface {
 	dealConn(c net.Conn)
 }
 
-func NewServer() {
+func NewServer() *server {
 	return &onlyServer
 }
 func init() {
+	HTTP_r, _ = regexp.Compile(`^HTTP`)
 	onlyServer = server{flag: true, Data: make(chan [config.MaxNumberOfData][]byte)}
 }
 func initData() {}
@@ -39,6 +49,12 @@ func dealError(err error) {
 	fmt.Println(err)
 }
 func (s server) start() error {
+	s.listen()
+	if s.Err != nil {
+		return s.Err
+	}
+	go s.accept()
+	return nil
 }
 func listen() (net.Listener, error) {
 	l, err := net.Listen("tcp", addr+":"+port)
@@ -58,4 +74,45 @@ func accept(l net.Listener, h connHandler) {
 		}
 		go h.dealConn(c)
 	}
+}
+func (s server) accept() {
+	accept(s.listener, s)
+}
+func (s server) dealConn(c net.Conn) {
+	data, err := read(c)
+	switch parse(data) {
+	case "HTTP":
+		dealHTTP(c)
+	}
+}
+func parse(data []byte) string {
+	fmt.Println(string(data))
+	if isHTTP(data) {
+		return "HTTP"
+	}
+	return "unkown"
+}
+func isHTTP(data []byte) bool {
+	if HTTP_r.Find(data) != nil {
+		return true
+	}
+	return false
+}
+func read(c net.Conn) ([]byte, error) {
+	var result []byte
+	buffer := make([]byte, 1024)
+	for {
+		n, err := c.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				dealError(err)
+				return nil, err
+			}
+		}
+		result = append(result, buffer[:n]...)
+		if n < 1024 {
+			break
+		}
+	}
+	return result, nil
 }
